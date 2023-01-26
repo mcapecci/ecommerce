140c0a157adae2ccb71fcb973586782f4fb1d8a5
Eva Capecci <ecapecci@segurosrivadavia.com>
2023-01-26 16:52:59 -0300

    Configuration

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+All changes related to e-commerce, with every new release, must be described here in a human readable
+form. Please keep the structure of this file as it is described here [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+We are following [Semantic Versioning](https://semver.org/spec/v2.0.0.html) for our standard to assign the version
+number with every new release cycle. 
+
+## [0.0.1]
+### Added
+- e-commerce is created.
+
---
+++ e-commerce-service/src/main/java/com/ecommerce/application/ApplicationUtils.java
@@ -0,0 +1,58 @@
+package com.ecommerce.application;
+
+import java.net.InetAddress;
+import java.util.Arrays;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.env.Environment;
+
+/**
+ * The class <code>com.ecommerce.controller.application.ApplicationUtils</code> is a util
+ * class for operations over applications.
+ * 
+ * @author Eva Magalí Capecci
+ *
+ */
+public final class ApplicationUtils {
+
+	/**
+	 * Unique constructor without arguments.
+	 */
+	private ApplicationUtils() {
+		super();
+	}
+
+	/**
+	 * Shows information about the application.
+	 *
+	 * @param env Environment.
+	 * @return Info.
+	 */
+	public static String logApplicationStartup(Environment env) {
+
+		String protocol = "http";
+		if (env.getProperty("server.ssl.key-store") != null) {
+			protocol = "https";
+		}
+		String serverPort = env.getProperty("server.port");
+		String contextPath = env.getProperty("server.servlet.context-path");
+		if (StringUtils.isBlank(contextPath)) {
+			contextPath = "/";
+		}
+		String hostAddress = "localhost";
+		try {
+			hostAddress = InetAddress.getLocalHost().getHostAddress();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return String.format(
+				"\n----------------------------------------------------------\n\t"
+						+ "Application '%s' is running! Access URLs:\n\t" + "Local: \t\t%s://localhost:%s%s\n\t"
+						+ "Swagger: /swagger-ui.html#\n\t" + "External: \t%s://%s:%s:%s\n\t"
+						+ "Profile(s): \t%s\n----------------------------------------------------------",
+				env.getProperty("spring.application.name"), protocol, serverPort, contextPath, protocol, hostAddress,
+				serverPort, contextPath, Arrays.toString(env.getActiveProfiles()));
+
+	}
+}
