5a1e939c90e02a0506d8841fa5438ad84c6a3571
Eva Capecci <ecapecci@segurosrivadavia.com>
2023-01-26 16:58:57 -0300

    DTO added

---
+++ e-commerce-api/src/main/java/com/ecommerce/dto/response/example/ExamplePagePricesResponseDto.java
@@ -0,0 +1,16 @@
+package com.ecommerce.dto.response.example;
+
+import com.ecommerce.dto.response.PageResponseDto;
+import com.ecommerce.dto.response.PricesResponseDto;
+
+/**
+ * ExamplePagePricesResponseDto
+ * 
+ * Swagger doesn't support generics. This class is just a workaround.
+ * 
+ * @author Eva Magalí Capecci
+ *
+ */
+@SuppressWarnings("serial")
+public class ExamplePagePricesResponseDto extends PageResponseDto<PricesResponseDto> {
+}
